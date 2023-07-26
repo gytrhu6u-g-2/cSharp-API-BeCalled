9f7cee389a27bad81c7f5063e7cfc2068dfaf1aa
gytrhu6u-g-2 <gytrhu6u@icloud.com>
2023-07-26 21:52:41 +0900

    API呼び出し

---
+++ API-BeCalled/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+/// に対して POST が来たら呼ばれる処理を登録
+app.MapPost("/", (int? one, int? two, RequestBody yourAnswer) =>
+{
+    if (one + two != 30)
+    {
+        return Results.Ok(new Response { Result = $"答えが違うよ! Your answer is {yourAnswer.Answer}" });
+    }
+
+    return Results.Ok(new Response { Result = $"正解！", Result2 = one + two });
+});
+
+app.Run();
+
+//リクエストとレスポンス
+class RequestBody
+{
+    public int? Answer { get; set; }
+}
+
+class Response
+{
+    public string? Result { get; set; }
+    public int? Result2 { get; set; }
+}
